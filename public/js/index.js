2acdc86865309e7ff2dbe60bdcac40be320454b7
rewantkedia <kediarewant@gmail.com>
2018-03-30 15:36:17 +0530

    add new message and create message events

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+var socket = io();
+socket.on('connect',function(){ //builtin event 'connect'
+    console.log('Connected to server');
+    socket.emit('createMessage',{
+        from: 'Rewant',
+        message: 'Hellow World'
+    })
+})
+socket.on('disconnect',function(){
+    console.log('Disconnected to server');
+})
+socket.on('newMessage',function(message){
+    console.log(message);
+})
--- server/server.js
+++ server/server.js
@@ -1,17 +1,36 @@
 const path = require('path');
 const express = require('express');
+const socketIO  = require('socket.io');
+const http = require('http');
 const public_path = path.join(__dirname,'/../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
-
+var server  = http.createServer(app);
 app.use(express.static(public_path));
+var io = socketIO(server);
+
+io.on('connection',(socket)=>{
+    console.log('New user connected. ');
+    socket.on('disconnect',()=>{
+        console.log('Disconnected from client');
+    })
+
+    socket.on('createMessage',(message)=>{
+        console.log('Create message',message);
+
+    })
+
+    socket.emit('newMessage',{
+    from: 'John',
+        text: 'New message',
+        createdAt:  123123})
+})
 
 
 
-
-
-
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log('Server is up and running in port ',port);
 })
+
+//https://evening-meadow-89856.herokuapp.com/
